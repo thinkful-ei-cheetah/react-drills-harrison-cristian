b80d51464d01f36004162d95bd0ce35c84311977
Harrison Chen <harrisonchen@harrisons-mbp.lan>
2019-04-17 14:31:24 -0700

    drill4 test

--- src/state-drills/Accordion.js
+++ src/state-drills/Accordion.js
@@ -1,52 +1,42 @@
-import React from "react";
+import React, { Component } from 'react'
 
-export default class Accordion extends React.Component {
-    static defaultProps = {
-        sections: [
-            {
-            title: 'Section 1',
-            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-            },
-            {
-            title: 'Section 2',
-            content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
-            },
-            {
-            title: 'Section 3',
-            content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
-            },
-        ]
-    }
 
-    state={
-        activeSection: null,
-    }
+export default class Accordion extends Component {
+  static defaultProps = {
+    sections: []
+  };
 
-    handleSetActiveSection = (sectionIndex) => {
-        this.setState({
-            activeSection: sectionIndex
-        })
-    }
+  state = {
+    activeSectionIndex: null,
+  }
 
-    renderDisplay(section, idx, activeSection) {
-            return (
-            <li className="accordion-item" key={idx}>
-            <button type="button" onClick={() => this.handleSetActiveSection(idx)}>{section.title}</button>
-            {(activeSection === idx) && <p>{section.content}</p>}
-            </li>
-            )
-        }
+  handleSetActiveSecion = (sectionIndex) => {
+    this.setState({ activeSectionIndex: sectionIndex })
+  }
 
-    render() {
-        const {activeSection} = this.state
-        const {sections} = this.props
+  renderItem(section, idx, activeSectionIndex) {
+    return (
+      <li className='Accordion__item' key={idx}>
+        <button
+          type='button'
+          onClick={() => this.handleSetActiveSecion(idx)}
+        >
+          {section.title}
+        </button>
+        {(activeSectionIndex === idx) && <p>{section.content}</p>}
+      </li>
+    )
+  }
 
-        return (
-            <ul className="Accordion">
-                {sections.map((section, idx) =>
-                    this.renderDisplay(section, idx, activeSection)
-                )}
-            </ul>
-        )
-    }
+  render() {
+    const { activeSectionIndex } = this.state
+    const { sections } = this.props
+    return (
+      <ul className='Accordion'>
+        {sections.map((section, idx) =>
+          this.renderItem(section, idx, activeSectionIndex)
+        )}
+      </ul>
+    )
+  }
 }
